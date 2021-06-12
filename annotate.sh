c2ba58f8e03535c24464a7f54d32c819f39c3488
Nicole Lin <73249434+nlin0907@users.noreply.github.com>
2021-06-12 16:23:10 -0400

    Add files via upload

---
+++ annotate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SDIR=/Users/nicolelin/biomart/analysis3
+DIR=/Users/nicolelin/biomart
+
+python $DIR/replaceunderscore.py $SDIR/ref-biomart.txt $SDIR/u_ref-biomart.txt
+
+python $DIR/calls_txt_to_bed.py $SDIR/calls.txt $SDIR/calls.bed
+sort-bed $SDIR/calls.bed > $SDIR/sorted_calls.bed
+
+cut -f 1-4 $SDIR/u_ref-biomart.txt > $SDIR/ref-biomart_gene.bed
+sort-bed $SDIR/ref-biomart_gene.bed > $SDIR/sorted_ref-biomart_gene.bed
+
+cut -f 1-3,5 $SDIR/u_ref-biomart.txt > $SDIR/ref-biomart_phenotype.bed
+sort-bed $SDIR/ref-biomart_phenotype.bed > $SDIR/sorted_ref-biomart_phenotype.bed
+
+cut -f 1-3,6 $SDIR/u_ref-biomart.txt > $SDIR/ref-biomart_mim.bed
+sort-bed $SDIR/ref-biomart_mim.bed > $SDIR/sorted_ref-biomart_mim.bed
+
+python $DIR/gff_parsing.py $DIR/DGV.GS.March2016.50percent.GainLossSep.Final.hg19.gff3 $DIR/DGVHG37.bed 
+sort-bed $DIR/DGVHG37.bed > $DIR/sorted_DGVHG37.bed
+
+bedmap --echo --echo-map-id-uniq $SDIR/sorted_calls.bed $SDIR/sorted_ref-biomart_gene.bed > $SDIR/anno_calls1.bed
+bedmap --echo --echo-map-id-uniq $SDIR/anno_calls1.bed $SDIR/sorted_ref-biomart_phenotype.bed > $SDIR/anno_calls2.bed
+bedmap --echo --echo-map-id-uniq $SDIR/anno_calls2.bed $SDIR/sorted_ref-biomart_mim.bed > $SDIR/anno_calls3.bed
+bedmap --echo --echo-map-id-uniq --fraction-both 0.75 $SDIR/anno_calls3.bed $DIR/sorted_DGVHG37.bed > $SDIR/annotated_calls.bed
+
+python $DIR/get_sampleID.py $SDIR/calls.txt $SDIR/sampleID_calls.bed
+sort-bed $SDIR/sampleID_calls.bed > $SDIR/sorted_sampleID_calls.bed
+
+bedmap --echo --echo-map-id-uniq --fraction-both 0.90 --count $SDIR/annotated_calls.bed $SDIR/sorted_sampleID_calls.bed > $SDIR/combined.bed
+
+python $DIR/replace_absolute_value.py $SDIR/combined.bed $SDIR/combined.txt
+
+python $DIR/final_step.py $SDIR/combined.txt $SDIR/sorted_sampleID_calls.bed $SDIR/FINAL_ANNOTATED_CALLS.txt
---
+++ biomart_analysis.R
@@ -0,0 +1,25 @@
+library(biomaRt)
+ensembl = useMart("ENSEMBL_MART_ENSEMBL", dataset="hsapiens_gene_ensembl", host="grch37.ensembl.org", path="/biomart/martservice")
+attributes<-c("chromosome_name", "start_position", "end_position", "hgnc_symbol", "strand", "mim_morbid_description", "phenotype_description")
+# gene
+# attributes<-c("chromosome_name", "start_position", "end_position", "hgnc_symbol")
+# phenotype
+# attributes<-c("chromosome_name", "start_position", "end_position", "phenotype_description")
+# mim
+# attributes<-c("chromosome_name", "start_position", "end_position", "mim_morbid_description")
+connection <- file("/Users/nicolelin/analysis3_calls.txt", open="r")
+lines <- readLines(connection)
+
+for (i in 1:length(lines)) {
+  line = lines[i]
+  line_elements <- scan(text=line, what = character(1), sep="\t")
+  chrom = line_elements[2]
+  chrom_in = sub(".*r", "", chrom)
+  start_in = line_elements[3]
+  stop_in = line_elements[4]
+  results <- getBM(attributes=attributes, filters=c("chromosome_name","start","end"), values=list(chrom_in, start_in, stop_in), mart=ensembl)
+  write.table(results, "/Users/nicolelin/ref-biomart_analysis3.txt", append = TRUE, sep = "\t", dec = ".", row.names = FALSE, col.names = FALSE)
+}
+
+close(connection)
+
